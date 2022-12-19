d22af071c5961f94add7a31c6eb3d085ba87818d
mustafagoek <mustafa.gok@deop.ca>
2022-12-19 01:55:43 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "mgok-testrg" {
+  name     = "test-rg"
+  location = "West Europe"
+}
---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.36.0"
+    }
+  }
+
+  backend "remote" {
+  organization = "MustafaG"
+
+  workspaces {
+    name = "T-1217-FirstWork"
+  }
+}
+}
+
+provider "azurerm" {
+  features {
+
+  }
+}
